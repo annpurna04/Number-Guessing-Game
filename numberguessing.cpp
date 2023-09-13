0edb15054c56efbc5beede818d04a0f551011dbb
annpurna04 <65756912+annpurna04@users.noreply.github.com>
2023-09-13 14:44:34 +0530

    numberguessing.cpp

--- numberguessing.cpp
+++ numberguessing.cpp
@@ -1,27 +1,32 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int num=num%100+1;
-    int guess,attempt;
-    cout<<"\t\t\t\t\t---IT'S A NUMBER GUESSING GAME---\n\n";
-    cout<<"\t\t\t\t\t\t!!LET'S START!!\n\n";
-    for(int i=1; i<=10; i++)
-     {    
-         cout<<"\t\t\tEnter some input numbers between(0 and 99) for guessing a number:\n\n"; 
-          cin>>guess;
-          attempt=i;
-     if(num==guess){
-     cout<<"Congrats! You guessed a correct number.";
-     break;
-}
-     else if(num<guess){
-    cout<<"Guess is high\n";
-    cout<<"Try Again\n\n";
-}
-    else
-    {
-    cout<<"Guess is low\n";
-    cout<<"Try Again\n\n";
-}
-}
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+    // Seed the random number generator
+    std::srand(static_cast<unsigned int>(std::time(NULL)));
+
+    int secretNumber = std::rand() % 100 + 1; // Generate a random number between 1 and 100
+    int guess;
+    int attempts = 0;
+
+    std::cout << "Welcome to the Number Guessing Game!\n";
+    std::cout << "I have selected a number between 1 and 100. Try to guess it.\n";
+
+    while (true) {
+        std::cout << "Enter your guess: ";
+        std::cin >> guess;
+        attempts++;
+
+        if (guess < secretNumber) {
+            std::cout << "Too low! Try again.\n";
+        } else if (guess > secretNumber) {
+            std::cout << "Too high! Try again.\n";
+        } else {
+            std::cout << "Congratulations! You guessed the number " << secretNumber << " in " << attempts << " attempts.\n";
+            break;
+        }
+    }
+
+    return 0;
 }
